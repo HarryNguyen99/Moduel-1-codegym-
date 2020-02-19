f8a4e33391722dae01b2c816714698345d0bb28d
harrynguyen99 <phuquyit@gmail.com>
2020-02-19 11:39:06 +0700

    su dung toan tu

---
+++ (TT) Su dung toan tu/SuDungToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("nhap chieu dai");
+inputHeight = prompt("nhap chieu rong");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("dien tich =" + area);
