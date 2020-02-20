4f07b2a153a9f9ab6a7f7cd7acca34caaf5cbec3
harrynguyen99 <phuquyit@gmail.com>
2020-02-20 20:18:30 +0700

    v1

---
+++ (BT)Cau lenh Switch-case/js/app.js
@@ -0,0 +1,23 @@
+
+    function tínhsongay() {
+        let month = document.getElementById("thang").value;
+        if (month >= 1 && month <= 12) {
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    document.getElementById("songay").innerHTML = "Tháng Có 31 Ngày";
+                    break;
+                case 2:
+                    document.getElementById("songay").innerHTML = "Tháng 2 Có 28, 29 Ngày";
+                default :
+                    document.getElementById("songay").innerHTML = "Tháng Có 30 Ngày";
+            }
+        } else
+            document.getElementById("songay").innerHTML = "Nhập Lại";
+
+    }
