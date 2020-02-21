4f6448209bcbc92c5a5c8aebf910e33578e97015
Harry Nguyen <phuquyit@gmail.com>
2020-02-21 09:19:25 +0700

    v1

--- (BT)May tinh don gian/js/my.js
+++ (BT)May tinh don gian/js/my.js
@@ -1,5 +1,51 @@
-function f1() {
-    let num1 = document.getElementById("so1").value;
-    let n1 = parseInt(num1);
-    document.getElementById("manhinh").innerHTML = n1;
+function f1(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f2(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f3(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f4(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f5(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f6(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f7(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f8(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f9(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function f0(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function fc(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function ft(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function fn(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function fccc(x) {
+    document.getElementById("manhinh").value +=x ;
+}
+function fcc() {
+    document.getElementById("manhinh").value = "" ;
+}
+
+function fb() {
+    let ketqua = eval();
+    document.getElementById("manhinh").value = ketqua;
+
 }
